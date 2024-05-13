9aae41e607d3cff98aea0eb68b7b0b51679663ef
Krish <131204733+worhasdev@users.noreply.github.com>
2024-05-13 10:33:53 +0100

    Create EmoteSchema.lua

---
+++ EmoteSchema.lua
@@ -0,0 +1,17 @@
+--[[
+
+]]--
+
+local EmoteData = {}
+
+-- Define emotes as a strata of EmoteData
+EmoteData.Emotes = {
+	{animationId = "17106083672", emoteName = "Emote 1", audioId = "1836049841"},
+	{animationId = "13931867608", emoteName = "Emote 2", audioId = "1841140365"},
+	{animationId = "13932021413", emoteName = "Emote 3", audioId = "1836049841"},
+	
+	
+	-- Add more emotes following this format
+}
+
+return EmoteData
